59fb4a86de8671db2ea07b1995866c6b99b01e2e
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-08 13:40:14 +0700

    update start.py

--- Ubot/modules/bot/start.py
+++ Ubot/modules/bot/start.py
@@ -47,6 +47,25 @@ MSG_BOT = """
 command_filter = filters.private & filters.command("buat") & filters.user(ADMINS) & ~filters.via_bot
 
 
+filename = ".env"
+
+if os.path.isfile(filename):
+    with open(filename, "r") as file:
+        contents = file.read()
+    session_list = re.findall(r"(SESSION\d+)=.*", contents)
+    session_index = len(session_list) + 1
+else:
+    session_index = 1
+
+@app.on_message(command_filter)
+async def create_env(client, message):
+    global session_index  # tambahkan ini
+    with open(filename, "a") as file:
+        file.write(f"\nSESSION{session_index}={message.text.split()[1]}")
+        load_dotenv()
+    await message.reply_text(f"Session berhasil disimpan pada {filename} dengan Posisi SESSION{session_index}.")
+    session_index += 1
+
 
 @app.on_message(filters.command(["alive"]))
 async def module_help(client: Client, message: Message):
