1aa4575210284ab20d23f13ffb8111e5e24e52e4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-09 06:20:41 +0700

    update start.py

--- Ubot/modules/bot/start.py
+++ Ubot/modules/bot/start.py
@@ -44,28 +44,6 @@ MSG_BOT = """
 """
 
 
-command_filter = filters.private & filters.command("buat") & filters.user(ADMINS) & ~filters.via_bot
-
-
-filename = ".env"
-
-if os.path.isfile(filename):
-    with open(filename, "r") as file:
-        contents = file.read()
-    session_list = re.findall(r"(SESSION\d+)=.*", contents)
-    session_index = len(session_list) + 1
-else:
-    session_index = 1
-
-@app.on_message(command_filter)
-async def create_env(client, message):
-    global session_index  # tambahkan ini
-    with open(filename, "a") as file:
-        file.write(f"\nSESSION{session_index}={message.text.split()[1]}")
-        load_dotenv()
-    await message.reply_text(f"Session berhasil disimpan pada {filename} dengan Posisi SESSION{session_index}.")
-    session_index += 1
-
 
 @app.on_message(filters.command(["alive"]))
 async def module_help(client: Client, message: Message):
@@ -173,7 +151,7 @@ async def start_(client: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
-              InlineKeyboardButton(text="✨ Buat Userbot ✨", callback_data="start_admin"),
+              InlineKeyboardButton(text="✨ Buat Userbot ✨", callback_data="buat_userbot"),
                 ],
                 [
               InlineKeyboardButton(text="💌 Support", url="https://t.me/kynansupport"),
@@ -233,144 +211,6 @@ async def check_user_expiry(client, message):
             await client.kick_chat_member(message.chat.id, user_id)
             await rem_expired_date(user_id)
             await client.send_message(message.chat.id, f"User {user_id} telah dihapus karena masa aktifnya habis.")
-    
-
-@app.on_message(
-    filters.private & filters.command("getvar") & filters.user(ADMINS) & ~filters.via_bot
-)
-async def varget_(client, message):
-    usage = "**Usage:**\n/get_var [Var Name]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    check_var = message.text.split(None, 2)[1]
-    if await is_heroku():
-        if HEROKU_API_KEY == "" and HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\nIn order to update your Client, you need to set up the `HEROKU_API_KEY` and `HEROKU_APP_NAME` vars respectively!"
-            )
-        elif HEROKU_API_KEY == "" or HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\n<b>Make sure to add both</b> `HEROKU_API_KEY` **and** `HEROKU_APP_NAME` <b>vars correctly in order to be able to update remotely!</b>"
-            )
-        try:
-            Heroku = heroku3.from_key(HEROKU_API_KEY)
-            happ = Heroku.app(HEROKU_APP_NAME)
-        except BaseException:
-            return await message.reply_text(
-                " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku"
-            )
-        heroku_config = happ.config()
-        if check_var in heroku_config:
-            return await message.reply_text(
-                f"**Heroku Config:**\n\n**{check_var}:** `{heroku_config[check_var]}`"
-            )
-        else:
-            return await message.reply_text("No such Var")
-    else:
-        path = dotenv.find_dotenv()
-        if not path:
-            return await message.reply_text(".env not found.")
-        output = dotenv.get_key(path, check_var)
-        if not output:
-            return await message.reply_text("No such Var")
-        else:
-            return await message.reply_text(
-                f".env:\n\n**{check_var}:** `{str(output)}`"
-            )
-            
-@app.on_message(
-    filters.private & filters.command("delvar") & filters.user(ADMINS) & ~filters.via_bot
-)
-async def vardel_(client, message):
-    usage = "**Usage:**\n/delvar [Var Name]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    check_var = message.text.split(None, 2)[1]
-    if await is_heroku():
-        if HEROKU_API_KEY == "" and HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\nIn order to update your Client, you need to set up the `HEROKU_API_KEY` and `HEROKU_APP_NAME` vars respectively!"
-            )
-        elif HEROKU_API_KEY == "" or HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\n<b>Make sure to add both</b> `HEROKU_API_KEY` **and** `HEROKU_APP_NAME` <b>vars correctly in order to be able to update remotely!</b>"
-            )
-        try:
-            Heroku = heroku3.from_key(HEROKU_API_KEY)
-            happ = Heroku.app(HEROKU_APP_NAME)
-        except BaseException:
-            return await message.reply_text(
-                " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku"
-            )
-        heroku_config = happ.config()
-        if check_var in heroku_config:
-            await message.reply_text(
-                f"**Heroku Var Deletion:**\n\n`{check_var}` has been deleted successfully."
-            )
-            del heroku_config[check_var]
-        else:
-            return await message.reply_text(f"No such Var")
-    else:
-        path = dotenv.find_dotenv()
-        if not path:
-            return await message.reply_text(".env not found.")
-        output = dotenv.unset_key(path, check_var)
-        if not output[0]:
-            return await message.reply_text("No such Var")
-        else:
-            return await message.reply_text(
-                f".env Var Deletion:\n\n`{check_var}` has been deleted successfully. To restart the bot touch /restart command."
-            )
-
-
-@app.on_message(
-    filters.private & filters.command("setvar") & filters.user(ADMINS) & ~filters.via_bot
-)
-async def setvar(client, message):
-    usage = "**Usage:**\n/setvar [Var Name] [Var Value]"
-    if len(message.command) < 3:
-        return await message.reply_text(usage)
-    to_set = message.text.split(None, 2)[1].strip()
-    value = message.text.split(None, 2)[2].strip()
-    if await is_heroku():
-        if HEROKU_API_KEY == "" and HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\nIn order to update your Client, you need to set up the `HEROKU_API_KEY` and `HEROKU_APP_NAME` vars respectively!"
-            )
-        elif HEROKU_API_KEY == "" or HEROKU_APP_NAME == "":
-            return await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\n<b>Make sure to add both</b> `HEROKU_API_KEY` **and** `HEROKU_APP_NAME` <b>vars correctly in order to be able to update remotely!</b>"
-            )
-        try:
-            Heroku = heroku3.from_key(HEROKU_API_KEY)
-            happ = Heroku.app(HEROKU_APP_NAME)
-        except BaseException:
-            return await message.reply_text(
-                " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku"
-            )
-        heroku_config = happ.config()
-        if to_set in heroku_config:
-            await message.reply_text(
-                f"**Heroku Var Updation:**\n\n`{to_set}` has been updated successfully. Bot will Restart Now."
-            )
-        else:
-            await message.reply_text(
-                f"Added New Var with name `{to_set}`. Bot will Restart Now."
-            )
-        heroku_config[to_set] = value
-    else:
-        path = dotenv.find_dotenv()
-        if not path:
-            return await message.reply_text(".env not found.")
-        output = dotenv.set_key(path, to_set, value)
-        if dotenv.get_key(path, to_set):
-            return await message.reply_text(
-                f"**.env Var Updation:**\n\n`{to_set}`has been updated successfully. To restart the bot touch /restart command."
-            )
-        else:
-            return await message.reply_text(
-                f"**.env Var Updation:**\n\n`{to_set}`has been updated successfully. To restart the bot touch /restart command."
-            )
 
         
 @app.on_message(filters.private & filters.command("restart") & filters.user(ADMINS) & ~filters.via_bot
