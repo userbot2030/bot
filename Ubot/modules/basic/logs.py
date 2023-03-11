820f994b5d034e129e47c6973e18cdf73bdf0718
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-12 02:41:15 +0700

    update logs.py

--- Ubot/modules/basic/logs.py
+++ Ubot/modules/basic/logs.py
@@ -15,8 +15,8 @@ collection = cli["Kyran"]["tag_log"]
 
 
 
-
-async def idup_log(user_id: int) -> bool:
+async def idup_log(user_id: int, message: Message) -> bool:
+    log = {"user_id": user_id}
     try:
         result = await collection.users.update_one(
             {'user_id': user_id},
@@ -24,12 +24,15 @@ async def idup_log(user_id: int) -> bool:
             upsert=True
         )
         if result.modified_count > 0 or result.upserted_id:
+            await message.edit("**Logger Tag Berhasil Dihidupkan**")
+            await log_tagged_messages()
             return True
     except:
         return False
 
 
-async def mati_log(user_id: int) -> bool:
+async def mati_log(user_id: int, message: Message) -> bool:
+    log = {"user_id": user_id}
     try:
         result = await collection.users.update_one(
             {'user_id': user_id},
@@ -37,42 +40,38 @@ async def mati_log(user_id: int) -> bool:
             upsert=True
         )
         if result.modified_count > 0 or result.upserted_id:
-            return True
+            await message.edit("**Logger Tag Berhasil Dimatikan**")
+            return False
     except:
         return False
-
+        
 
 @Client.on_message(filters.group & filters.mentioned & filters.incoming)
-async def log_tagged_messages(client: Client, message: Message):
-    user_id = message.from_user.id
-    user = await client.get_users(user_id)
-    if user and getattr(user, 'tag_log', False):
-        tai = f"<b>📨 #TAGS #MESSAGE</b>\n<b> • : </b>{user.mention}"
-        tai += f"\n<b> • Group : </b>{message.chat.title}"
-        tai += f"\n<b> • 👀 </b><a href='{message.link}'>Lihat Pesan</a>"
-        tai += f"\n<b> • Message : </b><code>{message.text}</code>"
-        await client.send_message(
-            BOTLOG_CHATID,
-            tai,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True,
-        )
+async def log_tagged_messages(client, message):
+  user_id = message.from_user.id
+  tai = f"<b>📨 #TAGS #MESSAGE</b>\n<b> • : </b>{message.from_user.mention}"
+  tai += f"\n<b> • Group : </b>{message.chat.title}"
+  tai += f"\n<b> • 👀 </b><a href='{message.link}'>Lihat Pesan</a>"
+  tai += f"\n<b> • Message : </b><code>{message.text}</code>"
+  await asyncio.sleep(0.1)
+  await client.send_message(
+      BOTLOG_CHATID,
+      tai,
+      parse_mode=enums.ParseMode.HTML,
+      disable_web_page_preview=True,
+  )
+
 
 
 @Client.on_message(filters.command("log", cmds) & filters.me)
 async def set_log(client, message):
     user_id = message.from_user.id
-    result = await idup_log(user_id)
-    if result:
-        await message.edit("**Logger Tag Berhasil Dihidupkan**")
-        await log_tagged_messages(client, message)
+    await idup_log(user_id, message)
 
 
 @Client.on_message(filters.command("nolog", cmds) & filters.me)
 async def set_no_log(client, message):
     user_id = message.from_user.id
-    result = await mati_log(user_id)
-    if result:
-        await message.edit("**Logger Tag Berhasil Dimatikan**")
+    await mati_log(user_id, message)
 
         
