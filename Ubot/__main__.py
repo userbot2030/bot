e5325a8d656abeac05b29ce5487272834d43eb07
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-11 11:11:46 +0700

    update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -106,10 +106,7 @@ async def main():
             await app.send_message(SUPPORT, MSG_BOT.format(py(), pyro, user))
             await idle()
             await install()
-
-
-              
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-LOGGER("Info").info("Stop Ubot Pyro")
+            
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    LOGGER("Info").info("Stop Ubot Pyro")
