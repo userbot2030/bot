5c04b422fed2caac6c5d76eff87c3980b77b4c28
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-12 09:49:25 +0700

    update logs.py

--- Ubot/modules/basic/logs.py
+++ Ubot/modules/basic/logs.py
@@ -8,7 +8,7 @@ from pyrogram.filters import chat
 import asyncio
 from typing import Dict, List, Union
 from datetime import datetime, timedelta
-from Ubot import BOTLOG_CHATID
+from Ubot import BOTLOG_CHATID, app
 log = []
 
 collection = cli["Kyran"]["tag_log"]
