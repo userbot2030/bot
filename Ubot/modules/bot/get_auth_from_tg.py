9c476d266bba5ce94bbac68fd8d9b8ffa744fe28
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 01:53:51 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -106,8 +106,8 @@ async def recv_tg_code_message(_, message: Message):
             with open(filename, "r") as file:
                 contents = file.read()
                 if sesi in contents:
-                   await message.reply_text(f"`Processing...`")
-                 return
+                        await message.reply_text(f"`Processing...`")
+                        return
                 else:
                     load_dotenv()
                     jumlah = next(session_counter)
