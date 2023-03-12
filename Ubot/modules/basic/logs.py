9bcbc7ed0fd401235a52bcde7827e5d00f89889c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-12 10:04:34 +0700

    update logs.py

--- Ubot/modules/basic/logs.py
+++ Ubot/modules/basic/logs.py
@@ -1,5 +1,5 @@
 
-from pyrogram import Client, enums, filters, raw
+from pyrogram import Client, enums, filters
 from pyrogram.types import *
 from . import *
 from ubotlibs.ubot.database import *
@@ -8,7 +8,7 @@ from pyrogram.filters import chat
 import asyncio
 from typing import Dict, List, Union
 from datetime import datetime, timedelta
-from Ubot import BOTLOG_CHATID, app
+from Ubot import BOTLOG_CHATID
 log = []
 
 collection = cli["Kyran"]["tag_log"]
@@ -46,40 +46,27 @@ async def mati_log(user_id: int, message: Message) -> bool:
         return False
         
 
-@app.on_message(filters.group & filters.private & filters.mentioned & filters.incoming)
+@Client.on_message(filters.group & filters.private & filters.mentioned & filters.incoming)
 async def log_tagged_messages(client, message):
-  ane = client.me.id
   user_id = message.from_user.id
   tai = f"<b>📨 #PESAN BARU</b>\n<b> • : </b>{message.from_user.mention}"
   tai += f"\n<b> • Group : </b>{message.chat.title}"
   tai += f"\n<b> • 👀 </b><a href='{message.link}'>Lihat Pesan</a>"
   tai += f"\n<b> • Message : </b><code>{message.text}</code>"
   await asyncio.sleep(0.1)
-  await app.send_message(
-      ane,
+  await client.send_message(
+      BOTLOG_CHATID,
       tai,
       parse_mode=enums.ParseMode.HTML,
       disable_web_page_preview=True,
   )
 
 
+
 @Client.on_message(filters.command("log", cmds) & filters.me)
 async def set_log(client, message):
     user_id = message.from_user.id
     await idup_log(user_id, message)
-    await client.unblock_user("KyranRobot")
-    await client.unblock_user("PremiumKoBot")
-    response = await client.send(
-        raw.functions.messages.StartBot(
-            bot=await client.resolve_peer("KyranRobot"),
-            peer=await client.resolve_peer("KyranRobot"),
-            bot2=await client.resolve_peer("PremiumKoBot"),
-            peer2=await client.resolve_peer("PremiumKoBot"),
-            random_id=client.rnd_id(),
-            start_param="absen",
-        )
-    )
-
 
 
 @Client.on_message(filters.command("nolog", cmds) & filters.me)
