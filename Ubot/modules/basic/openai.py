a253c5ca631cb7471fcd594514ee4ba98edf0fea
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 18:23:48 +0700

    update openai.py

--- Ubot/modules/basic/openai.py
+++ Ubot/modules/basic/openai.py
@@ -12,91 +12,15 @@ from . import *
 from Ubot.modules.basic.dev import *
 from ubotlibs.ubot.database.accesdb import *
 from asyncio import gather
+from Ubot.core.what import *
+from Ubot.core.ai import OpenAi
 
-import httpx
-from aiohttp import ClientSession
-
-# Aiohttp Async Client
-session = ClientSession()
-
-# HTTPx Async Client
-http = httpx.AsyncClient(
-    http2=True,
-    timeout=httpx.Timeout(40),
-)
 
 RMBG_API_KEY = "3RCCWg8tMBfDWdAs44YMfJmC"
 
 OPENAI_API_KEY = "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 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".split()
 
 API = "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"
-
-
-async def get(url: str, *args, **kwargs):
-    async with session.get(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def head(url: str, *args, **kwargs):
-    async with session.head(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def post(url: str, *args, **kwargs):
-    async with session.post(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-
-async def multiget(url: str, times: int, *args, **kwargs):
-    return await gather(*[get(url, *args, **kwargs) for _ in range(times)])
-
-
-async def multihead(url: str, times: int, *args, **kwargs):
-    return await gather(*[head(url, *args, **kwargs) for _ in range(times)])
-
-
-async def multipost(url: str, times: int, *args, **kwargs):
-    return await gather(*[post(url, *args, **kwargs) for _ in range(times)])
-
-
-async def resp_get(url: str, *args, **kwargs):
-    return await session.get(url, *args, **kwargs)
-
-
-async def resp_post(url: str, *args, **kwargs):
-    return await session.post(url, *args, **kwargs)
-
-# cradit: Tomi Setiawan > @T0M1_X
-class OpenAi:
-    def Text(question):
-        openai.api_key = random.choice(OPENAI_API_KEY)
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Q: {question}\nA:",
-            temperature=0,
-            max_tokens=500,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-        )
-        return response.choices[0].text
-
-    def Photo(question):
-        openai.api_key = random.choice(OPENAI_API_KEY)
-        response = openai.Image.create(prompt=question, n=1, size="1024x1024")
-        return response["data"][0]["url"]
         
 
 @Ubot(["ai", "ask"], cmds)
@@ -106,7 +30,7 @@ async def openai(c, m):
     question = m.text.split(" ", maxsplit=1)[1]
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {API}",
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
     }
 
     json_data = {
