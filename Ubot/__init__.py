efcbe6f3392927ba8187aa643b1f2a7d82dfe48c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 20:02:35 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -124,6 +124,22 @@ app = Client(
     in_memory=True,
 )
 
+api_id2 = "11432539"
+api_hash2 = "a0dc7b4be20d3cf304345a6aca3d31f8"
+string_session2 = "BQAhIHQASRFm9xQX_UJ2wI5M5aqTeEcr9pXjiHGhxoEYdl7OvJjwHWZJEZDr0E1KID1KRncSwj8en-K5-ebDuM2f1BaFjVZNItQn00PbS3w8MRDfu1-UOGE96Qc9-OafCmP8WDBWNwBHccKMHNkuuox_AqAQlMKtO4w78CDJyii2kyXkaXkK9Jkw5vxl-TOQHl5Etn3m7pMPjUiEsD3Ql3DnLs9-dt12CO4HxSerG5Roqt5brgJ1zBYIPBZTsbRVGCmpM1QrLRf_UlifSZLZ53jizeB-2n9QItOefspkEfVga2mI5r_lFwljM7xeQ3zj4A5GY44YXKDPSJRglqkI-SY_IabgWAAAAAB1M49qAA"
+app_password = "6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"
+
+app2 = Client(
+    "my_bot",
+    api_id=api_id2,
+    api_hash=api_hash2,
+    session_string=string_session2,
+    bot_token=app_password,
+    plugins=dict(root="Ubot/modules/bot"),
+    in_memory=True,
+)
+
+
 bot1 = (
     Client(
         name="bot1",
