7a9bb5b9d0bd4994c85668d00c57f13d04a82cff
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 22:43:21 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -24,7 +24,8 @@ import dotenv
 from dotenv import load_dotenv
 from Ubot.logging import LOGGER
 from os import environ, execle
-from itertools import count
+import itertools
+
 from ubotlibs.ubot.database.accesdb import *
 HAPP = None
 
