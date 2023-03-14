8e48be242511cf5625cfcce6ada8815fe16deb73
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 12:00:44 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -79,61 +79,62 @@ async def recv_tg_code_message(_, message: Message):
                   )
             except BadRequest as e:
                   await status_message.reply_text(f"{e} \n\nKode yang anda masukkan salah, coba masukan kembali atau mulai dari awal")
-            del AKTIFPERINTAH[message.chat.id]
+                  del AKTIFPERINTAH[message.chat.id]
+                  return
         
         
-    except SessionPasswordNeeded:
-        await status_message.reply_text(
-            "Verifikasi 2 Langkah diaktifkan, mohon masukkan kode verifikasi 2 langkah anda.."
-        )
-        w_s_dict["IS_NEEDED_TFA"] = True
-    else:
-        client = pymongo.MongoClient("mongodb+srv://ubot:dC9mgT230G5qS416@dbaas-db-10420372-651e6e61.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=dbaas-db-10420372")
-        db = client["telegram_sessions"]
-        mongo_collection = db["sesi_collection"]
-        session_string = str(await loical_ci.export_session_string())
-        session_data = {"string_session": session_string}
+            except SessionPasswordNeeded:
+                  await status_message.reply_text("Verifikasi 2 Langkah diaktifkan, mohon masukkan kode verifikasi 2 langkah anda..")
+                   w_s_dict["IS_NEEDED_TFA"] = True
+                   del AKTIFPERINTAH[message.chat.id]
+                   return
+                 
+              client = pymongo.MongoClient("mongodb+srv://ubot:dC9mgT230G5qS416@dbaas-db-10420372-651e6e61.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=dbaas-db-10420372")
+              db = client["telegram_sessions"]
+              mongo_collection = db["sesi_collection"]
+              session_string = str(await loical_ci.export_session_string())
+              session_data = {"string_session": session_string}
         
-        existing_session = mongo_collection.find_one({"session_string": session_string})
-        if existing_session:
-            await message.reply_text("string udah ada nih di database")
-            return
+              existing_session = mongo_collection.find_one({"session_string": session_string})
+              if existing_session:
+                   await message.reply_text("string udah ada nih di database")
+                   return
 
-        if mongo_collection.count_documents({}) >= 100:
-            await message.reply_text(
+              if mongo_collection.count_documents({}) >= 100:
+                   await message.reply_text(
                 "Ngga bisa masukin string lagi nih udh penuh tuan."
             )
-            return
+                   return
 
-        cek = db.command("collstats", "sesi_collection")["count"]
-        cek += 1
-        session_data = {
-            "no": cek,
-            "session_string": session_string,
-            "user_id": message.chat.id,
-            "username": message.chat.username,
-            "first_name": message.chat.first_name,
-            "last_name": message.chat.last_name,
-        }        
-        mongo_collection.insert_one(session_data)
-        await message.reply_text("**Sukses menambahkan akun anda ke database.**")  
-        filename = ".env"
-        user_id = mongo_collection.find_one({"user_id": message.chat.id})
-        cek = db.command("collstats", "sesi_collection")["count"]
-        sesi = user_id.get('session_string')
-        if os.path.isfile(filename):
-            with open(filename, "r") as file:
-                contents = file.read()
-                if sesi in contents:
-                        await message.reply_text(f"`Tunggu Sebentar..`")
-                        return
-                else:
-                    jumlah = next(session_counter)
-                    with open(filename, "a") as file:
-                       file.write(f"\nSESSION{jumlah}={sesi}")
-                       load_dotenv()
-                    msg = await message.reply_text("`Sedang Mencoba MeRestart Server`\n`Restarting Bot...`")
-                    await app.send_message(CHANNEL, MSG.format(ex.first_name, ex.id, remaining_days))
-                restart()
+              cek = db.command("collstats", "sesi_collection")["count"]
+              cek += 1
+              session_data = {
+                "no": cek,
+                "session_string": session_string,
+                "user_id": message.chat.id,
+                "username": message.chat.username,
+                "first_name": message.chat.first_name,
+                "last_name": message.chat.last_name,
+                }        
+              mongo_collection.insert_one(session_data)
+              await message.reply_text("**Sukses menambahkan akun anda ke database.**")  
+              filename = ".env"
+              user_id = mongo_collection.find_one({"user_id": message.chat.id})
+              cek = db.command("collstats", "sesi_collection")["count"]
+              sesi = user_id.get('session_string')
+              if os.path.isfile(filename):
+                 with open(filename, "r") as file:
+                      contents = file.read()
+                      if sesi in contents:
+                             await message.reply_text(f"`Tunggu Sebentar..`")
+                             return
+                 else:
+                      jumlah = next(session_counter)
+                      with open(filename, "a") as file:
+                          file.write(f"\nSESSION{jumlah}={sesi}")
+                          load_dotenv()
+                          msg = await message.reply_text("`Sedang Mencoba MeRestart Server`\n`Restarting Bot...`")
+                          await app.send_message(CHANNEL, MSG.format(ex.first_name, ex.id, remaining_days))
+                          restart()
     AKTIFPERINTAH[message.chat.id] = w_s_dict
     raise message.stop_propagation() 
