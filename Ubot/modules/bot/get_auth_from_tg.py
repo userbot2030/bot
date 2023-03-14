7f3969b5adc4c20e5b2e7e0a867c323c2b9bc7a4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 12:16:09 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -75,7 +75,7 @@ async def recv_tg_code_message(_, message: Message):
            )
     except BadRequest as e:
            await status_message.reply_text(f"{e} \n\nKode yang anda masukkan salah, coba masukan kembali atau mulai dari awal")
-    del AKTIFPERINTAH[message.chat.id]
+           del AKTIFPERINTAH[message.chat.id]
            
     except SessionPasswordNeeded:
         await status_message.reply_text(
