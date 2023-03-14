3bfec4cbaba026feb95ab62d413d6ddec42a53ad
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 11:45:31 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -61,27 +61,27 @@ async def recv_tg_code_message(_, message: Message):
             if not w_s_dict:
                 return
               
-    sent_code = w_s_dict.get("SENT_CODE_R")
-    phone_number = w_s_dict.get("PHONE_NUMBER")
-    loical_ci = w_s_dict.get("USER_CLIENT")
-    if not sent_code or not phone_number:
-        return
-    status_message = w_s_dict.get("MESSAGE")
-    if not status_message:
-        return
-    del w_s_dict["MESSAGE"]
-    phone_code = "".join(message.text.split(" "))
-    try:
-        w_s_dict["SIGNED_IN"] = await loical_ci.sign_in(
-            phone_number,
-            sent_code.phone_code_hash,
-            phone_code
-        )
-    except BadRequest as e:
-        await status_message.reply_text(
-          f"{e} \n\nKode yang anda masukkan salah, coba masukan kembali atau mulai dari awal"
-        )
+            sent_code = w_s_dict.get("SENT_CODE_R")
+            phone_number = w_s_dict.get("PHONE_NUMBER")
+            loical_ci = w_s_dict.get("USER_CLIENT")
+             if not sent_code or not phone_number:
+                return
+            status_message = w_s_dict.get("MESSAGE")
+            if not status_message:
+               return
+            del w_s_dict["MESSAGE"]
+            phone_code = "".join(message.text.split(" "))
+        try:
+            w_s_dict["SIGNED_IN"] = await loical_ci.sign_in(
+              phone_number,
+              sent_code.phone_code_hash,
+              phone_code
+              )
+        except BadRequest as e:
+            await status_message.reply_text(f"{e} \n\nKode yang anda masukkan salah, coba masukan kembali atau mulai dari awal")
         del AKTIFPERINTAH[message.chat.id]
+        
+        
     except SessionPasswordNeeded:
         await status_message.reply_text(
             "Verifikasi 2 Langkah diaktifkan, mohon masukkan kode verifikasi 2 langkah anda.."
