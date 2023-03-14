bcfb2ae77dbb34a4e63cb0bd66107ae2a52aca4c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 07:29:35 +0700

    update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -121,10 +121,10 @@ async def recv_tg_code_message(_, message: Message):
                     LOGGER(__name__).info(f"{err}")
                     return
                   
-                  await msg.edit_text("✅ **Bot Berhasil DiRestart.\n**Tunggu 2 menit dan cek pesan tersimpan anda.**")
-                  if HAPP is not None:
-                     HAPP.restart()
-                  else:
+                await msg.edit_text("✅ **Bot Berhasil DiRestart.\n**Tunggu 2 menit dan cek pesan tersimpan anda.**")
+                if HAPP is not None:
+                    HAPP.restart()
+                else:
                     args = [sys.executable, "-m", "Ubot"]
                     execle(sys.executable, *args, environ)
                         
