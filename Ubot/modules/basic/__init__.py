734c38fee75ab70f909e507efbe6fb5dcb46f0c9
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 09:28:07 +0700

    update __init__.py

--- Ubot/modules/basic/__init__.py
+++ Ubot/modules/basic/__init__.py
@@ -1,5 +1,7 @@
 from pyrogram import Client, filters
 from Ubot import cmds
+import os
+import sys
 from Ubot.modules.basic.help import add_command_help
 from ubotlibs import BOT_VER
 from ubotlibs.ubot import Ubot, Devs
