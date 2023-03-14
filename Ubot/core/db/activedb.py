1df1d420a466172b7d9f1e6f747e48c1276f05e4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 08:45:16 +0700

    update activedb.py

--- Ubot/core/db/activedb.py
+++ Ubot/core/db/activedb.py
@@ -22,9 +22,6 @@ async def get_active_time(user_id):
     else:
         return None
 
-File "/root/bot/Ubot/core/db/activedb.py", line 26, in get_expired_date
-    user = await collection.users.find_one({"_id": user_id})
-AttributeError: 'list' object has no attribute 'users'
 
 async def get_expired_date(user_id):
     user = await collection.users.find_one({"_id": user_id})
