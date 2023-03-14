56813679835a00eeb87af670a57e74d6e349dddd
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-14 08:25:38 +0700

    update activedb.py

--- Ubot/core/db/activedb.py
+++ Ubot/core/db/activedb.py
@@ -2,10 +2,10 @@ from pyrogram.filters import chat
 from . import cli
 from typing import Dict, List, Union
 from datetime import datetime, timedelta
-from ubotlibs.ubot.database.accesdb import *
+from .accesdb import *
 import schedule
 import asyncio
-collection = cli["Kyran"]["active"]
+collection = ["expire_date"]
 
 
 
@@ -13,14 +13,23 @@ async def get_active_time(user_id):
     expire_date = await get_expired_date(user_id)
     if expire_date:
         active_time = expire_date - datetime.now()
-        return active_time
+        if active_time.total_seconds() > 0:
+            return active_time
+        else:
+            await delete_user_access(user_id)
+            await rem_expired_date(user_id)
+            return None
     else:
         return None
 
 async def get_expired_date(user_id):
     user = await collection.users.find_one({"_id": user_id})
     if user:
-        return user.get("expire_date")
+        expire_date = user.get("expire_date")
+        if expire_date:
+            return expire_date
+        else:
+            return None
     else:
         return None
 
@@ -35,11 +44,17 @@ async def remove_expired():
         await rem_expired_date(user["_id"])
 
 
-async def set_expired_date(user_id, expire_date):
+async def set_expired_date(user_id, active_time):
+    if active_time == 1:
+        expire_date = datetime.now() + relativedelta(months=1)
+    else:
+        expire_date = datetime.now() + timedelta(days=30)
     collection.users.update_one({"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True)
     schedule.every().day.at("00:00").do(remove_expired)
-    async def schedule_loop():
-        while True:
-            await asyncio.sleep(1)
-            schedule.run_pending()
     asyncio.create_task(schedule_loop())
+
+
+async def schedule_loop():
+    while True:
+        await asyncio.sleep(1)
+        schedule.run_pending()
