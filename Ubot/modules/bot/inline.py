59341872373577339914784d21e54d57340b4c7b
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-16 03:42:25 +0700

    update inline.py

--- Ubot/modules/bot/inline.py
+++ Ubot/modules/bot/inline.py
@@ -93,7 +93,6 @@ async def alive_function(message, answers):
     buttons = support()
     ex = await message._client.get_me()
     user = len(ids)
-    user_active_time = await get_active_time(ex.id)
     await message._client.invoke(Ping(ping_id=0))
     ping = (datetime.now() - start).microseconds / 1000
     uptime = await get_readable_time((time.time() - StartTime))
