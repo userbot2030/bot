021e2689485a500a96f1b74ed8f9c41256d5db88
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-15 14:33:48 +0700

    update config.py

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ load_dotenv(".env1")
 API_ID = int(getenv("API_ID", "22156937")) #optional
 API_HASH = getenv("API_HASH", "0f8f66b06b1c53b9263bcfb1123e9c85") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+DEEP_AI = getenv("DEEP_AI", "d7394561-0528-4714-a1ee-edd7020b48e1")
 OWNER_ID = int(getenv("OWNER_ID") or 0)
 ADMIN1_ID = list(map(int, getenv("ADMIN1_ID", "951454060").split()))
 ADMIN2_ID = list(map(int, getenv("ADMIN2_ID", "2099942562").split()))
