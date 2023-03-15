03eb2a07180871dc9b82c9e0f831819099ee2e62
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-15 19:43:19 +0700

    update profile.py

--- Ubot/modules/basic/profile.py
+++ Ubot/modules/basic/profile.py
@@ -12,7 +12,7 @@ import os
 from asyncio import sleep
 import os
 from . import *
-from ubotlibs.ubot.utils.misc import *
+
 from ubotlibs.ubot.helper.PyroHelpers import ReplyCheck
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -22,7 +22,87 @@ from ubotlibs.ubot.database.accesdb import *
 flood = {}
 profile_photo = "cache/pfp.jpg"
 
+async def list_admins(client: Client, chat_id: int):
+    global admins_in_chat
+    if chat_id in admins_in_chat:
+        interval = time() - admins_in_chat[chat_id]["last_updated_at"]
+        if interval < 3600:
+            return admins_in_chat[chat_id]["data"]
 
+    admins_in_chat[chat_id] = {
+        "last_updated_at": time(),
+        "data": [
+            member.user.id
+            async for member in client.get_chat_members(
+                chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+            )
+        ],
+    }
+    return admins_in_chat[chat_id]["data"]
+
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+
+    text = text.strip()
+
+    if is_int(text):
+        return int(text)
+
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+    
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
 
 @Ubot("unblock", cmds)
 async def unblock_user_func(client: Client, message: Message):
