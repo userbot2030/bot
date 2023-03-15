c25015d9a3c7c0f4a4b03359f00ae4f04e9a69c5
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-16 01:21:53 +0700

    update usersdb.py

--- Ubot/core/db/usersdb.py
+++ Ubot/core/db/usersdb.py
@@ -18,3 +18,35 @@ async def remove_user(user_id):
 
 async def get_active_users():
     return [doc["_id"] async for doc in collection.find()]
+
+
+async def add_ubot(user_id, api_id, api_hash, session_string):
+    return await collection.update_one(
+        {"user_id": user_id},
+        {
+            "$set": {
+                "api_id": api_id,
+                "api_hash": api_hash,
+                "session_string": session_string,
+            }
+        },
+        upsert=True,
+    )
+
+
+async def remove_ubot(user_id):
+    return await collection.delete_one({"user_id": user_id})
+
+
+async def get_userbots():
+    data = []
+    async for ubot in collection.find({"user_id": {"$exists": 1}}):
+        data.append(
+            dict(
+                name=str(ubot["user_id"]),
+                api_id=ubot["api_id"],
+                api_hash=ubot["api_hash"],
+                session_string=ubot["session_string"],
+            )
+        )
+    return data
