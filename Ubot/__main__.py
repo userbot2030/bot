33d1f1b3e2d19a5ea7875d333e2b2063a5e6e64b
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-16 01:49:47 +0700

    update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -23,6 +23,7 @@ MSG_BOT = """
 **New Ubot Actived ✅**
 **Phython**: `{}`
 **Pyrogram**: `{}`
+**User**: `{}`
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 """
 
@@ -41,7 +42,6 @@ MSG_ON = """
 async def main():
     await app.start()
     LOGGER("Ubot").info("Memulai Ubot Pyro..")
-    LOGGER("Ubot").info("Loading Everything.")
     for all_module in ALL_MODULES:
         importlib.import_module("Ubot.modules" + all_module)
     for bot in bots:
@@ -51,29 +51,18 @@ async def main():
             await join(bot)
             LOGGER("Ubot").info("Startup Completed")
             LOGGER("√").info(f"Started as {ex.first_name} | {ex.id} ")
-            await add_user(ex.id)
             ids.append(ex.id)
-            
             user = len(ids)
-        except Exception as e:
-            LOGGER("X").info(f"{e}")
-            if "AUTH_KEY_UNREGISTERED" in str(e):
-                load_dotenv()
-                session_name = None
-                for i in range(1, 201):
-                    if os.getenv(f"SESSION{i}") == str(e):
-                        session_name = f"SESSION{i}"
-                        os.environ.pop(session_name)
-                        LOGGER("Ubot").info(f"Removed {session_name} from .env file due to error.")
-                        await app.send_message(SUPPORT, f"Removed {session_name} from .env file due to error.")
-                        break
-                if session_name is None:
-                   LOGGER("Ubot").info(f"Could not find session name in .env file for error: {str(e)}")
-    await app.send_message(SUPPORT, MSG_BOT.format(py(), pyro))
+        except RPCError:
+            user_id = int(bot["name"])
+            await remove_ubot(user_id, bot["session"])
+            await app.send_message(
+                SUPPORT, f"✅ {bot['name']} Berhasil Dihapus Dari Database"
+            )
+            LOGGER("√").info(f"✅ {bot['name']} Berhasil Dihapus Dari Database")
+    await app.send_message(SUPPORT, MSG_BOT.format(py(), pyro, user))
     await idle()
     await aiosession.close()
-    for ex_id in ids:
-        await remove_user(ex_id)
 
 
               
