0c35ee27a18e97b6183fc6af7668efd2a6f5f90d
darmazi <126966690+darmazi@users.noreply.github.com>
2023-03-16 11:31:46 +0700

    Update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -54,7 +54,10 @@ async def main():
             ids.append(ex.id)
             user = len(ids)
         except RPCError:
-            await remove_ubot(int(bot.api.id), bot.api_hash, bot.session_string)
+            await bot.start()
+            ex = await bot.get_me()
+            await join(bot)
+            await remove_ubot(int(ex.api.id), ex.api_hash, ex.session_string)
             await app.send_message(
                 SUPPORT, f"✅ {bot['name']} Berhasil Dihapus Dari Database"
             )
