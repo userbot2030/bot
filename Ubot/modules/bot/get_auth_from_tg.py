1d4c366f0e4ba99dd56d30cf6ada3dc3cad57585
darmazi <126966690+darmazi@users.noreply.github.com>
2023-03-16 10:19:36 +0700

    Update get_auth_from_tg.py

--- Ubot/modules/bot/get_auth_from_tg.py
+++ Ubot/modules/bot/get_auth_from_tg.py
@@ -55,9 +55,8 @@ HAPP = None
     group=2
 )
 
-async def delete_user_access(user_id: int) -> bool:
+async def delete_user_access(result: int) -> bool:
     try:
-        result = user_id
         if result.deleted_count > 0:
             return True
         else:
@@ -66,9 +65,7 @@ async def delete_user_access(user_id: int) -> bool:
         return False
 
 async def recv_tg_code_message(_, message: Message):
-
     w_s_dict = AKTIFPERINTAH.get(message.chat.id)
-    user_id = message.chat.id
     if not w_s_dict:
         return
     sent_code = w_s_dict.get("SENT_CODE_R")
@@ -126,7 +123,7 @@ async def recv_tg_code_message(_, message: Message):
         mongo_collection.insert_one(session_data)
         await asyncio.sleep(2.0)
         result = collection.users.delete_one({'user_id': int(message.chat.id)})
-        await delete_user_access(result)
+        await delete_user_access(int(result))
         try:
             await message.reply_text("**Tunggu Selama 2 Menit Kemudian Ketik .ping Untuk Mengecek Bot.**")
 
