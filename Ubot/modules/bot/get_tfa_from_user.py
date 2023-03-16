3448f4cebf4dcba9e34bc5fd193a3d503ad8f5b8
darmazi <126966690+darmazi@users.noreply.github.com>
2023-03-16 09:53:39 +0700

    Update get_tfa_from_user.py

--- Ubot/modules/bot/get_tfa_from_user.py
+++ Ubot/modules/bot/get_tfa_from_user.py
@@ -120,7 +120,7 @@ async def recv_tg_tfa_message(_, message: Message):
         }        
         mongo_collection.insert_one(session_data)
         await asyncio.sleep(2.0)
-        await delete_user_access(message.chat.id)
+        await delete_user_access()
         try:
             await message.reply_text("**Tunggu Selama 2 Menit Kemudian Ketik .ping Untuk Mengecek Bot.**")
             LOGGER(__name__).info("BOT SERVER RESTARTED !!")
