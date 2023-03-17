3df34b3219ac3276010139c9df04e136e270ba22
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-17 13:55:29 +0700

    update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ ADMIN6_ID.append(2067434944)
 ADMIN7_ID.append(1694909518)
 
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ubot0:ubot0@ubot.zhj1x91.mongodb.net/?retryWrites=true&w=majority")
-BOT_TOKEN = getenv("BOT_TOKEN", "6257433800:AAHdLRslfAzNtLWScKcf_Tg4lRdxw7_cB1E")
+BOT_TOKEN = getenv("BOT_TOKEN", "5971028351:AAFQqxr9eEeTug5L6sGta4zNjPXGCrGZkMw")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER", True)
