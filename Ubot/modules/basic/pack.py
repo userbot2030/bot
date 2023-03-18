05afc018c0b6df123e7f01bb3618d6207e828a2c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-18 07:12:26 +0700

    update pack.py

--- Ubot/modules/basic/pack.py
+++ Ubot/modules/basic/pack.py
@@ -3,12 +3,9 @@ import random
 from glob import glob
 from pyrogram import Client, filters, types
 from pyrogram.errors import PeerIdInvalid
-from pyrogram.raw.functions import UploadMediaRequest
-
-from pyrogram.types import InputPeerSelf, InputStickerSetID, InputStickerSetItem
+from pyrogram.raw.functions.messages import UploadMediaRequest
+from pyrogram.types import InputPeerSelf, InputStickerSetID, InputStickerSetItem, InputMediaUploadedDocument, InputMediaUploadedPhoto
 from . import *
-
-
 
 async def get_input_documents(client, files):
     inputs = []
@@ -37,7 +34,7 @@ async def get_input_documents(client, files):
             await client.progress(0, count, f"Uploaded {count} files.")
     return inputs
 
-@Client.on_message(filters.command("packkang", cmds) & filters.me)
+@Client.on_message(filters.command("packkang") & filters.reply)
 async def pack_kangish(client, message):
     _e = message.reply_to_message
     local = None
@@ -47,6 +44,19 @@ async def pack_kangish(client, message):
         cmdtext = None
     if cmdtext and os.path.isdir(cmdtext):
         local = True
+    elif not (_e and _e.sticker and _e.document.mime_type == "image/webp"):
+        return await message.reply_text("Balas pesan stiker atau sertakan direktori yang valid.")
+    msg = await message.reply_text("Membuat set stiker...")
+    _packname = cmdtext or f"Kang Pack By {message.from_user.id}"
+    typee = None
+    if not local:
+        try:
+            _get_stiks = await client.get_sticker_set(_e.sticker.set_name)
+            docs = _get_stiks.documents
+        except PeerIdInvalid:
+            return await msg.edit(f"Bot tidak memiliki akses ke set stiker ini.")
+    else:
+        docs = []
         files = glob(cmdtext + "/*")
         exte = files[-1]
         if exte.endswith(".tgs"):
@@ -54,41 +64,17 @@ async def pack_kangish(client, message):
         elif exte.endswith(".webm"):
             typee = "vid"
         docs = await get_input_documents(client, files)
-    elif not (_e and _e.sticker and _e.document.mime_type == "image/webp"):
-        return await message.reply_text("Balas pesan stiker atau sertakan direktori yang valid.")
-    else:
-        docs = []
-        try:
-            _get_stiks = await client.get_sticker_set(_e.sticker.set_name)
-            for i in _get_stiks.documents:
-                docs.append(get_input_document(i))
-        except PeerIdInvalid:
-            return await msg.edit(f"Bot tidak memiliki akses ke set stiker ini.")
 
-    msg = await message.reply_text("Membuat set stiker...")
-    _packname = cmdtext or f"Kang Pack By {message.from_user.id}"
     stiks = []
     for i in docs:
-        if isinstance(i, InputMediaUploadedPhoto):
-            x = await client.send(UploadMediaRequest(InputPeerSelf(), i.file))
-            stiks.append(
-                InputStickerSetItem(
-                    document=x,
-                    emoji=random.choice(["😐", "👍", "😂"])
-                    if local
-                    else (i.caption or "👍"),
-                )
+        stiks.append(
+            InputStickerSetItem(
+                document=i,
+                emoji=random.choice(["😐", "👍", "😂"])
+                if local
+                else (i.attributes[1]).alt,
             )
-        elif isinstance(i, InputMediaUploadedDocument):
-            stiks.append(
-                InputStickerSetItem(
-                    document=i,
-                    emoji=random.choice(["😐", "👍", "😂"])
-                    if local
-                    else (i.caption or "👍"),
-                )
-            )
-
+        )
     try:
         short_name = "kang_" + _packname.replace(" ", "_") + str(message.message_id)
         _r_e_s = await client.create_sticker_set(
