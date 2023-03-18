52e94263a4416150e3f7a88850c3340a9f70c95e
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-18 16:39:54 +0700

    update pref.py

--- Ubot/core/db/pref.py
+++ Ubot/core/db/pref.py
@@ -7,24 +7,3 @@ from pymongo import MongoClient
 from typing import List
 
 
-client = MongoClient(MONGO_URL)
-cli = client["ubot"]
-collection = cli["prefix"]
-
-
-
-async def set_prefix(user_id, prefix):
-    pref = collection.get(f"user.{user_id}", "prefix", ".")
-    if prefix != pref:
-        collection.set(f"user.{user_id}", "prefix", prefix)
-        return True
-    return False
-
-
-def get_prefix(func):
-    async def wrapper(client, message):
-        user_id = message.from_user.id
-        pref = collection.get(f"user.{user_id}", "prefix", ".")
-        prefix = pref if pref is not None else ""
-        return await func(client, message, prefix)
-    return wrapper
