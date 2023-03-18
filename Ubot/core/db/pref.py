45b70d60ed164f517340be414c512cfc4533848d
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-18 15:00:00 +0700

    update pref.py

--- Ubot/core/db/pref.py
+++ Ubot/core/db/pref.py
@@ -8,26 +8,24 @@ from typing import List
 
 client = MongoClient(MONGO_URL)
 cli = client["ubot"]
-
 collection = cli["prefix"]
-
-
-
-
 
 async def get_prefix(user_id: int) -> str:
     result = await collection.find_one({"user_id": user_id})
     if result is None:
-        return None
+        return "."
     else:
-        return result.get("prefix", "")
-
+        return result.get("prefix", ".")
 
 async def set_prefix(user_id: int, prefix: str) -> None:
+    if prefix is None:
+        prefix = ""
     collection.update_one({"user_id": user_id}, {"$set": {"prefix": prefix}}, upsert=True)
-
 
 async def get_users_with_prefix() -> List[int]:
     results = await collection.find({"prefix": {"$ne": ""}})
     return [result["user_id"] for result in results]
+
+prefix = await get_prefix("core.main", "prefix", ".")
+
 
