9efd95e750e0e0c5ebd4ee1d488102ac07ad4aeb
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-21 21:04:37 +0700

    update face.py

--- Ubot/modules/basic/face.py
+++ Ubot/modules/basic/face.py
@@ -28,7 +28,7 @@ from . import *
 DEEP_AI = "d7394561-0528-4714-a1ee-edd7020b48e1"
 
 
-@Ubot(["Face", "face"], "")
+@Ubot(["face"], "")
 async def face_detect(c: Client, m: Message):
     pro = await m.reply_text("`Processing...`")
     await asyncio.sleep(5)
@@ -55,7 +55,7 @@ async def face_detect(c: Client, m: Message):
     except BaseException:
         pass
 
-@Ubot(["pcil", "Pcil"], "")
+@Ubot(["pcil"], "")
 async def generate_sketch(c: Client, m: Message):
     if m.reply_to_message.photo:
         file_id = m.reply_to_message.photo
@@ -75,7 +75,7 @@ async def generate_sketch(c: Client, m: Message):
 
 
 
-@Ubot(["toonify", "Toonify"], "")
+@Ubot(["toonify"], "")
 async def toonify_handler(c: Client, m: Message):
     pro = await m.reply_text("`Processing...`")
     await asyncio.sleep(5)
