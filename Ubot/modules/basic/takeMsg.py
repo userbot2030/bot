9b8ffdcaa76d0688e741cc511b971ff007f3ddc6
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-21 23:02:25 +0700

    update takeMsg.py

--- Ubot/modules/basic/takeMsg.py
+++ Ubot/modules/basic/takeMsg.py
@@ -6,7 +6,7 @@ from ubotlibs.ubot.database.accesdb import *
 from pyrogram.enums import MessagesFilter
 
 
-@Ubot(["take", "Take"], "")
+@Ubot(["take"], "")
 async def _(client, message):
     results = {
         "photo": MessagesFilter.PHOTO,
