d3be4433d674a309485311cfb1484dabde02c827
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-23 09:23:13 +0700

    Create logo.py

---
+++ Ubot/modules/basic/logo.py
@@ -0,0 +1,23 @@
+# Credits : @Xtsea
+
+from . import *
+
+from Ubot.core.lgs import *
+
+@Ubot("logo", "")
+
+async def logo_command(client, message):
+
+    await logo_write(client, message)
+
+add_command_help(
+
+    "logo",
+
+    [
+
+        [f"logo [kata]", "Buat Logo Secara Random."],
+
+    ],
+
+)
