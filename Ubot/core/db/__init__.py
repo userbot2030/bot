37abdc8aff9b9cb02a6853845d8d9f163d19d7d1
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-23 21:32:16 +0700

    update __init__.py

--- Ubot/core/db/__init__.py
+++ Ubot/core/db/__init__.py
@@ -7,16 +7,5 @@ from Ubot import *
 
 import sqlite3
 
-def create_database(client_name):
-    conn = sqlite3.connect(f"{client_name}.db")
-    conn.close()
 
-
-create_database(bot1.name)
-create_database(bot2.name)
-create_database(bot3.name)
-create_database(bot4.name)
-create_database(bot5.name)
-create_database(bot6.name)
-create_database(bot7.name)
 
