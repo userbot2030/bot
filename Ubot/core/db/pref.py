102856b904afd3808887eeadde9b289f009778bb
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-25 00:10:44 +0700

    Delete pref.py

--- Ubot/core/db/pref.py
+++
@@ -1,9 +0,0 @@
-
-from . import cli
-from config import MONGO_URL
-from pyrogram.types import Message
-from pyrogram import Client, filters
-from pymongo import MongoClient
-from typing import List
-
-
