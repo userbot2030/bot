dd660ff30c90ff7d32797489c70419bdfd8618c2
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-25 03:16:48 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -44,34 +44,6 @@ TEMP_SETTINGS: Dict[Any, Any] = {}
 TEMP_SETTINGS["PM_COUNT"] = {}
 TEMP_SETTINGS["PM_LAST_MSG"] = {}
 
-LOG_FILE_NAME = "logs.txt"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
-        logging.StreamHandler(),
-    ],
-)
-logging.getLogger("asyncio").setLevel(logging.CRITICAL)
-logging.getLogger("pytgcalls").setLevel(logging.WARNING)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
-logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
-logging.basicConfig(level=logging.INFO)
-
-LOGS = logging.getLogger(__name__)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
-
-if not BOT_TOKEN:
-   LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
-   sys.exit()
-
 LOOP = asyncio.get_event_loop_policy()
 event_loop = LOOP.get_event_loop()
 asyncio.set_event_loop(event_loop)
