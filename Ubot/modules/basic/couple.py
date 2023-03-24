87f8730d4aefbbddaaf7b61a90cc558ec58f63a6
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-25 00:00:36 +0700

    update couple.py

--- Ubot/modules/basic/couple.py
+++ Ubot/modules/basic/couple.py
@@ -32,7 +32,7 @@ tomorrow = str(dt_tom())
 
 
 @Ubot(["couple", "couples"], "")
-async def couple(_, message):
+async def couple(client, message):
     if message.chat.type == ChatType.PRIVATE:
         return await message.reply_text("Perintah ini hanya dapat digunakan dalam grup.")
     try:
@@ -40,7 +40,7 @@ async def couple(_, message):
         is_selected = await get_couple(chat_id, today)
         if not is_selected:
             list_of_users = []
-            async for i in pbot.get_chat_members(message.chat.id, limit=50):
+            async for i in client.get_chat_members(message.chat.id, limit=50):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
@@ -49,27 +49,27 @@ async def couple(_, message):
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
                 c1_id = random.choice(list_of_users)
-            c1_mention = (await pbot.get_users(c1_id)).mention
-            c2_mention = (await pbot.get_users(c2_id)).mention
+            c1_mention = (await client.get_users(c1_id)).mention
+            c2_mention = (await client.get_users(c2_id)).mention
 
             couple_selection_message = f"""**Pasangan hari ini :**
 
 {c1_mention} + {c2_mention} = 😘
 __Pasangan baru hari ini dapat dipilih pada jam 12 pagi {tomorrow}__"""
-            await pbot.send_message(message.chat.id, text=couple_selection_message)
+            await client.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
 
         elif is_selected:
             c1_id = int(is_selected["c1_id"])
             c2_id = int(is_selected["c2_id"])
-            c1_name = (await pbot.get_users(c1_id)).first_name
-            c2_name = (await pbot.get_users(c2_id)).first_name
+            c1_name = (await client.get_users(c1_id)).first_name
+            c2_name = (await client.get_users(c2_id)).first_name
             couple_selection_message = f"""Pasangan hari ini :
 
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 😘
 __Pasangan baru hari ini dapat dipilih pada jam 12 pagi {tomorrow}__"""
-            await pbot.send_message(message.chat.id, text=couple_selection_message)
+            await client.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
         print(e)
         await message.reply_text(e)
