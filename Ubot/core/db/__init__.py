e03d176022b42cde0aee3611018c565972976d95
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-27 00:31:15 +0700

    update __init__.py

--- Ubot/core/db/__init__.py
+++ Ubot/core/db/__init__.py
@@ -440,20 +440,20 @@ async def get_tagalert_status(user_id: int):
         
 async def blacklisted_chats() -> list:
     chats_list = []
-    async for chat in blacklist_chatdb.find({"chat_id": {"$lt": 0}}):
-        chats_list.append(chat["chat_id"])
+    async for chat in blacklist_chatdb.find({"user_id": user_id}, {"chat_id": {"$lt": 0}}):
+        chats_list.append(chat["user_id", "chat_id"])
     return chats_list
 
 
 async def blacklist_chat(chat_id: int) -> bool:
-    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.insert_one({"chat_id": chat_id})
+    if not await blacklist_chatdb.find_one({"user_id": user_id}, {"chat_id": chat_id}):
+        await blacklist_chatdb.insert_one({"user_id": user_id}, {"chat_id": chat_id})
         return True
     return False
 
 
 async def whitelist_chat(chat_id: int) -> bool:
-    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.delete_one({"chat_id": chat_id})
+    if await blacklist_chatdb.find_one({"user_id": user_id}, {"chat_id": chat_id}):
+        await blacklist_chatdb.insert_one({"user_id": user_id}, {"chat_id": chat_id})
         return True
     return False
