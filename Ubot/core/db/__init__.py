d2e44f310879461d1e1313e471ef3cfd8aa3fdec
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-27 02:02:48 +0700

    update __init__.py

--- Ubot/core/db/__init__.py
+++ Ubot/core/db/__init__.py
@@ -435,23 +435,23 @@ async def get_tagalert_status(user_id: int):
     else:
         return True
         
+        
+        
 async def blacklisted_chats(user_id: int) -> list:
     chats_list = []
     async for chat in blchatdb.users.find({"user_id": user_id, "chat_id": {"$lt": 0}}):
         chats_list.append({"user_id": chat["user_id"], "chat_id": chat["chat_id"]})
     return chats_list
 
-
-
 async def blacklist_chat(user_id: int, chat_id: int) -> bool:
-    if not await blchatdb.users.find_one({"user_id": user_id}, {"chat_id": chat_id}):
-        await blchatdb.users.insert_one({"user_id": user_id}, {"chat_id": chat_id})
+    if not await blchatdb.users.find_one({"user_id": user_id, "chat_id": chat_id}):
+        await blchatdb.users.insert_one({"user_id": user_id, "chat_id": chat_id})
         return True
     return False
 
 
 async def whitelist_chat(user_id: int, chat_id: int) -> bool:
-    if await blchatdb.users.find_one({"user_id": user_id}, {"chat_id": chat_id}):
-        await blchatdb.users.insert_one({"user_id": user_id}, {"chat_id": chat_id})
+    if await blchatdb.users.find_one({"user_id": user_id, "chat_id": chat_id}):
+        await blchatdb.users.insert_one({"user_id": user_id, "chat_id": chat_id})
         return True
     return False
