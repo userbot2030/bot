7828d51921e2a26712716ba068581b7bf79e07df
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-29 03:01:57 +0700

    Add files via upload

--- Ubot/core/SQL/globals.py
+++ Ubot/core/SQL/globals.py
@@ -1,53 +1,54 @@
-async def addgvar(user_id, variable, value):
-    if multi_globals.find_one({"user_id": user_id, "variable": str(variable)}):
-        await delgvar(user_id, variable)
-    multi_globals.insert_one({"user_id": user_id, "variable": str(variable), "value": value})
+from . import BASE, SESSION
+from sqlalchemy import Column, String, UnicodeText, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.exc import NoResultFound
 
 
-async def delgvar(user_id, variable):
-    rem = multi_globals.delete_one({"user_id": user_id, "variable": str(variable)})
+
+class Globals(BASE):
+    __tablename__ = "globals"
+    user_id = Column(String(14), primary_key=True)
+    variable = Column(String, primary_key=True, nullable=False)
+    value = Column(UnicodeText, primary_key=True, nullable=False)
+
+    def __init__(self, variable, value, user_id):
+        self.user_id = (user_id)
+        self.variable = str(variable)
+        self.value = value
+        
+
+
+Globals.__table__.create(checkfirst=True)
+
+
+def ambil_grup(user_id):
+    try:
+        return SESSION.query(Globals).filter(Globals.user_id == user_id, Globals.variable == "GRUPLOG").one().value
+    except NoResultFound:
+        return None
+
+def addgvar(user_id, variable, value):
+    if SESSION.query(Globals).filter(Globals.user_id == user_id, Globals.variable == str(variable)).one_or_none():
+        delgvar(user_id, variable)
+    adder = Globals(user_id=user_id, variable=str(variable), value=value)
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def delgvar(user_id, variable):
+    rem = (
+        SESSION.query(Globals)
+        .filter(Globals.user_id == user_id, Globals.variable == str(variable))
+        .delete(synchronize_session="fetch")
+    )
     if rem:
-        return rem.deleted_count
+        SESSION.commit()
 
 
-async def gvarstatus(user_id, variable):
-    res = multi_globals.find_one({"user_id": user_id, "variable": str(variable)})
-    return res["value"] if res else None
-    
-
-@Client.on_message(
-    filters.private & filters.incoming & ~filters.service & ~filters.me & ~filters.bot
-)
-async def monito_p_m_s(client, message):
-    chat_id = message.chat.id
-    user_id = client.me.id
-    
-    # tambahkan baris berikut
-    if not await gvarstatus(str(user_id), "MULTI_MODE"):
-        return
-    
-    botlog_chat_id = await get_botlog(user_id)
-    if await gvarstatus(str(user_id), "PMLOG") == "false":
-        return
-    if not no_log_pms_sql.is_approved(message.chat.id) and message.chat.id != 777000:
-        if LOG_CHATS_.RECENT_USER != message.chat.id:
-            LOG_CHATS_.RECENT_USER = message.chat.id
-            if LOG_CHATS_.NEWPM:
-                await LOG_CHATS_.NEWPM.edit(
-                    LOG_CHATS_.NEWPM.text.replace(
-                        "**💌 PESAN BARU**",
-                        f" • `{LOG_CHATS_.COUNT}` **Pesan**",
-                    )
-                )
-                LOG_CHATS_.COUNT = 0
-            LOG_CHATS_.NEWPM = await client.send_message(
-                botlog_chat_id,
-                f"💌 <b><u>MENERUSKAN PESAN BARU</u></b>\n<b> • Dari :</b> {message.from_user.mention}\n<b> • User ID :</b> <code>{message.from_user.id}</code>",
-                parse_mode=enums.ParseMode.HTML,
-            )
-        try:
-            async for pmlog in client.search_messages(message.chat.id, limit=1):
-                await pmlog.forward(botlog_chat_id)
-            LOG_CHATS_.COUNT += 1
-        except BaseException:
-            pass
+def gvarstatus(user_id, variable):
+    try:
+        return SESSION.query(Globals).filter(Globals.user_id == user_id, Globals.variable == str(variable)).first()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
