763c0e66afd4fb6e11b2f36491aa0c10dd19db27
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 02:50:07 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -17,6 +17,7 @@ from gpytranslate import Translator
 from pyrogram import Client, filters
 from pytgcalls import GroupCallFactory
 from Ubot.get_config import get_config
+from Ubot.bot import Bot
 from config import *
 cmds = None
 CMD_HELP = {}
@@ -123,15 +124,7 @@ SESSION_GENERATED_USING = get_config("SESSION_GENERATED_USING", (
 
 
 
-app = Client(
-    name="ubot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    sleep_threshold=5,
-    plugins=dict(root="Ubot/modules/bot"),
-    in_memory=True,
-)
+app = Bot()
 
 
 bot1 = (
