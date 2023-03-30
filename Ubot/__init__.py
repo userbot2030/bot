9fe52c1bec2dd56426c7b74d2d1639ebef995633
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 20:05:50 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -118,15 +118,7 @@ SESSION_GENERATED_USING = get_config("SESSION_GENERATED_USING", (
 
 
 
-app = Client(
-    name="ubot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    sleep_threshold=5,
-    plugins=dict(root="Ubot/modules/bot"),
-    in_memory=True,
-)
+app = Bot()
 
 
 bot1 = (
