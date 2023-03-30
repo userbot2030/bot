28de5aad77b4366bb6c03e4a9954dc46fb23bcd1
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 11:18:08 +0700

    update apm.py

--- Ubot/modules/basic/apm.py
+++ Ubot/modules/basic/apm.py
@@ -133,7 +133,9 @@ async def reply_pm(client, message):
             await set.allow_user(user_id, chat_id) 
             await client.send_message(
                 message.chat.id,
-                f"<b>Menerima Pesan!!!</b>\n [Anda](tg://user?id={chat_id}) <b>Terdeteksi Developer Naya-Project</b>")
+                f"<b>Menerima Pesan!!!</b>\n{message.from_user.mention} <b>Terdeteksi Developer Naya-Premium</b>",
+                parse_mode=enums.ParseMode.HTML,
+            )
         except:
             pass
         return
