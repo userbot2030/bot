72cd0ee498c1e57ee8f2f431da30e00f2d096c05
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 12:12:33 +0700

    update user.py

--- Ubot/user.py
+++ Ubot/user.py
@@ -437,15 +437,3 @@ class User30(Client):
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped. Bye.")
-        
-        async def start(self):
-        await super().start()
-        usr_bot_me = self.me
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username} based on Pyrogram v{__version__} "
-        )
-        return (self, usr_bot_me.id)
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
