8f8c22de6e577661c2972434efe7fc6b0e9472c0
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-31 01:21:55 +0700

    update inline.py

--- Ubot/core/inline.py
+++ Ubot/core/inline.py
@@ -43,10 +43,10 @@ def paginate_help(page_number, loaded_modules, prefix):
                 InlineKeyboardButton(
                     text="❯", callback_data=f"{prefix}_next({modulo_page})"
                 ),
-            )
+            ),
             (
                 InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close_help"),
-            )
+            ),
         ]
     return pairs
 
