322308552ca732aa390330043dafc7b8e869c94c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-03 00:42:25 +0700

    update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -1,6 +1,9 @@
 
-
-from Ubot.core.db.mongo import db
-
-
-prefix = db.get("core.main", "prefix", ".")
+from .ai import *
+from .data import *
+from .func import *
+from .inline import *
+from .lgs import *
+from .what import *
+from .filter import *
+from .constans import *
