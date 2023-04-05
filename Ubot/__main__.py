abbec4ee9486ba5e68517c3075b563d9b54e6eb3
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-05 20:42:04 +0700

    update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -6,7 +6,7 @@ from pyrogram import idle
 from pyrogram.errors import RPCError
 from uvloop import install
 from ubotlibs import *
-from Ubot import aiosession, bots, app, ids, LOOP
+from Ubot import aiosession, bots, app, ids, event_loop
 from platform import python_version as py
 from Ubot.logging import LOGGER
 from pyrogram import __version__ as pyro
@@ -49,19 +49,20 @@ async def main():
         try:
             await bot.start()
             ex = await bot.get_me()
+            await join(bot)
             user_id = ex.id
             await buat_log(bot)
             botlog_chat_id = await get_botlog(user_id)
-            LOGGER("Ubot").info("Startup Completed")
-            LOGGER("√").info(f"Started as {ex.first_name} | {ex.id} ")
-            await join(bot)
-            user = len(ids)
-            await bot.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py(), pyro))
+            try:
+                await bot.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py(), pyro))
+            except BaseException as a:
+                LOGGER("Info").warning(f"{a}")
+            LOGGER("Info").info("Startup Completed")
+            LOGGER("✓").info(f"Started as {ex.first_name} | {ex.id} ")
             ids.append(ex.id)
         except Exception as e:
             LOGGER("X").info(f"{e}")
-    
-#    await app.send_message(SUPPORT, MSG_BOT.format(py(), pyro, user))
+            
     await idle()
     await aiosession.close()
     await app.stop()
@@ -74,4 +75,4 @@ async def main():
 if __name__ == "__main__":
     LOGGER("Ubot").info("Starting  Ubot")
     install()
-    LOOP.run_until_complete(main())
+    event_loop.run_until_complete(main())
